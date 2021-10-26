af74908a5e0cc4f48aa71193e5063f612c22fe19
adlrusu <AdrianLeontin.Rusu@endava.com>
2021-10-26 23:44:25 +0300

    - rough sketch of the ui part - added the adapter for a trip preview

---
+++ app/src/main/java/com/example/cityguide/presentation/trips/TripPreviewAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.cityguide.presentation.trips
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cityguide.R
+import com.example.cityguide.data.db.Trip
+import com.example.cityguide.databinding.TripsPreviewListItemBinding
+
+class TripPreviewAdapter
+    : ListAdapter<Trip, TripPreviewAdapter.TripPreviewViewHolder>(TripPreviewComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TripPreviewViewHolder {
+        val binding =
+            TripsPreviewListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return TripPreviewViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TripPreviewViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        if (currentItem != null)
+            holder.bind(currentItem)
+    }
+
+    class TripPreviewViewHolder(private val binding: TripsPreviewListItemBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(trip: Trip) {
+                binding.apply {
+                    tripItemTitle.text = itemView.context.getString(
+                        R.string.trip_item_title_format,
+                        trip.address.city,
+                        trip.address.country_code
+                    )
+
+                    trip.dateStart?.also {
+                        tripPeriod.text = itemView.context.getString(
+                            R.string.trip_item_period_format,
+                            it,
+                            trip.dateEnd
+                        )
+                    } ?: run {
+                        tripPeriod.text = ""
+                    }
+                }
+            }
+        }
+
+    class TripPreviewComparator : DiffUtil.ItemCallback<Trip>() {
+        override fun areItemsTheSame(oldItem: Trip, newItem: Trip): Boolean
+            = oldItem.xid == newItem.xid
+
+        override fun areContentsTheSame(oldItem: Trip, newItem: Trip): Boolean
+            = oldItem == newItem
+    }
+}
---
+++ app/src/main/java/com/example/cityguide/presentation/trips/TripsPreviewList.kt
@@ -0,0 +1,12 @@
+package com.example.cityguide.presentation.trips
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cityguide.R
+
+class TripsPreviewList : Fragment(R.layout.fragment_trips_preview_list) {
+
+}
