6f4e81a95a7b36be0514df5efa5c6bb7eb838263
Teodor Ganga <redplanet.mailbox@gmail.com>
2021-10-27 17:46:20 +0300

    Conected Splash screen -> Main page

--- app/src/main/java/com/example/cityguide/MainActivity.kt
+++ app/src/main/java/com/example/cityguide/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        getSupportActionBar()?.hide()
+        supportActionBar?.hide()
 
         setContentView(R.layout.activity_main)
 
