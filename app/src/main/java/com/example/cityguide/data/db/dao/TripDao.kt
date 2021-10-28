6c6edfca8ca01c4673056ab28073c707ec181ee9
adlrusu <AdrianLeontin.Rusu@endava.com>
2021-10-28 16:35:41 +0300

    tried to switch over to rx, vm still crashes

--- app/src/main/java/com/example/cityguide/data/db/dao/TripDao.kt
+++ app/src/main/java/com/example/cityguide/data/db/dao/TripDao.kt
@@ -2,6 +2,8 @@ package com.example.cityguide.data.db.dao
 
 import androidx.room.*
 import com.example.cityguide.data.db.entity.Trip
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Observable
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -9,19 +11,19 @@ interface TripDao {
 
     @Transaction
     @Query("SELECT * FROM trip")
-    fun getAllTrips(): Flow<List<Trip>>
+    fun getAllTrips(): Observable<List<Trip>>
 
     @Transaction
     @Query("SELECT * FROM trip WHERE trip.dateStart <= (SELECT strftime('%s', 'now')) AND (SELECT strftime('%s', 'now')) <= trip.dateEnd")
-    fun getActiveTrips(): Flow<List<Trip>>
+    fun getActiveTrips(): Observable<List<Trip>>
 
     @Transaction
     @Query("SELECT * FROM trip WHERE trip.dateStart IS NULL OR trip.dateEnd IS NULL OR trip.dateStart >= (SELECT strftime('%s', 'now'))")
-    fun getUpcomingTrips(): Flow<List<Trip>>
+    fun getUpcomingTrips(): Observable<List<Trip>>
 
     @Transaction
     @Query("SELECT * FROM trip WHERE (SELECT strftime('%s', 'now')) >= trip.dateEnd")
-    fun getCompletedTrips(): Flow<List<Trip>>
+    fun getCompletedTrips(): Observable<List<Trip>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTrips(trips: List<Trip>)
--- app/src/main/java/com/example/cityguide/presentation/trips/TripViewModel.kt
+++ app/src/main/java/com/example/cityguide/presentation/trips/TripViewModel.kt
@@ -1,15 +1,49 @@
 package com.example.cityguide.presentation.trips
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import com.example.cityguide.data.db.entity.Trip
 import com.example.cityguide.data.repository.TripRepository
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import javax.inject.Inject
 
 class TripViewModel @Inject constructor(
-    repository: TripRepository
+    private val repository: TripRepository
 ) : ViewModel() {
 
-    val activeTrips = repository.getActiveTrips().asLiveData()
-    val upcomingTrips = repository.getUpcomingTrips().asLiveData()
-    val completedTrips = repository.getCompletedTrips().asLiveData()
+    val activeTrips = MutableLiveData<List<Trip>>()
+    val upcomingTrips = MutableLiveData<List<Trip>>()
+    val completedTrips = MutableLiveData<List<Trip>>()
+
+
+    private val activeTripsCompositeDisposable = CompositeDisposable()
+    private val upcomingTripsCompositeDisposable = CompositeDisposable()
+    private val completedTripsCompositeDisposable = CompositeDisposable()
+
+    init {
+        getTrips()
+    }
+
+    private fun getTrips() {
+        activeTripsCompositeDisposable.add(
+            repository.getActiveTrips().observeOn(AndroidSchedulers.mainThread())
+                .subscribe { result -> activeTrips.value = result }
+        )
+        upcomingTripsCompositeDisposable.add(
+            repository.getActiveTrips().observeOn(AndroidSchedulers.mainThread())
+                .subscribe { result -> activeTrips.value = result }
+        )
+        completedTripsCompositeDisposable.add(
+            repository.getActiveTrips().observeOn(AndroidSchedulers.mainThread())
+                .subscribe { result -> activeTrips.value = result }
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        activeTripsCompositeDisposable.clear()
+        upcomingTripsCompositeDisposable.clear()
+        completedTripsCompositeDisposable.clear()
+    }
 }
