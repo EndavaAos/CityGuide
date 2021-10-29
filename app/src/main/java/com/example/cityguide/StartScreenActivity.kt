b29428507bb8f8d343debc0d6617fd20fc24053d
adlrusu <AdrianLeontin.Rusu@endava.com>
2021-10-29 11:03:43 +0300

    merged with with POISssscreen

--- app/src/main/java/com/example/cityguide/StartScreenActivity.kt
+++ app/src/main/java/com/example/cityguide/StartScreenActivity.kt
@@ -31,7 +31,7 @@ class StartScreenActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         fragmentManager.beginTransaction()
-            .replace(R.id.main_fragment_container, searchFragment)
+            .replace(R.id.main_fragment_holder, searchFragment)
             .commit()
     }
 
@@ -39,13 +39,13 @@ class StartScreenActivity : AppCompatActivity() {
         when(menu.itemId) {
             R.id.nav_home -> {
                 fragmentManager.beginTransaction()
-                    .replace(R.id.main_fragment_container, searchFragment)
+                    .replace(R.id.main_fragment_holder, searchFragment)
                     .commit()
                 return true
             }
             R.id.nav_mytrip -> {
                 fragmentManager.beginTransaction()
-                    .replace(R.id.main_fragment_container, tripsFragment)
+                    .replace(R.id.main_fragment_holder, tripsFragment)
                     .commit()
                 return true
             }
--- app/src/main/java/com/example/cityguide/di/AppModule.kt
+++ app/src/main/java/com/example/cityguide/di/AppModule.kt
@@ -36,12 +36,6 @@ class AppModule(
 
     @Singleton
     @Provides
-    fun providesRecyclerViewAdapter(context: Context): RecyclerViewAdapter {
-        return RecyclerViewAdapter(context)
-    }
-
-    @Singleton
-    @Provides
     fun providesLocationRepository(locationApi: LocationApi): LocationRepository {
         return LocationRepositoryImpl(locationApi)
     }
