da325368faea02476b95aa96efb34835c194e5cd
adrianrusu16 <72539388+adrianrusu16@users.noreply.github.com>
2021-11-01 18:30:17 +0200

    lists now receive their view models instead of the observable data type

--- app/src/main/java/com/example/cityguide/presentation/trips/tripSegment/GeneralTripViewModel.kt
+++ app/src/main/java/com/example/cityguide/presentation/trips/tripSegment/GeneralTripViewModel.kt
@@ -2,13 +2,18 @@ package com.example.cityguide.presentation.trips.tripSegment
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cityguide.data.db.entity.Trips
+import com.example.cityguide.data.repository.TripRepository
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-abstract class GeneralTripViewModel : ViewModel() {
+abstract class GeneralTripViewModel(
+    private val repository: TripRepository
+) : ViewModel() {
 
     abstract val data: Observable<List<Trips>>
 
@@ -22,6 +27,10 @@ abstract class GeneralTripViewModel : ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe { result -> trips.value = result }
         )
+    }
+
+    fun onTripSwiped(trip: Trips) = viewModelScope.launch {
+        repository.deleteTrip(trip)
     }
 
     override fun onCleared() {
