4603195de35e5911815c5137870f9f9127a74b4b
Eduard <edy_pricop@yahoo.com>
2021-11-09 22:00:02 +0200

    time picker set notification

--- app/src/main/java/com/example/cityguide/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/cityguide/presentation/settings/SettingsFragment.kt
@@ -1,23 +1,34 @@
 package com.example.cityguide.presentation.settings
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.text.format.DateFormat.is24HourFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
+import com.example.cityguide.AlarmReceiver
 import com.example.cityguide.R
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
+import kotlinx.android.synthetic.main.fragment_make_trip.*
 import kotlinx.android.synthetic.main.fragment_settings.*
 import kotlinx.android.synthetic.main.fragment_settings.view.*
+import java.util.*
 
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
-    lateinit var amPm: String
-    lateinit var minString: String
-    lateinit var hString: String
+    lateinit var calendar: Calendar
+    lateinit var alarmManager: AlarmManager
+    lateinit var pendingIntent: PendingIntent
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +36,7 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
-
+        createNotificationChannel()
         view.setTimeButton.setOnClickListener {
             openTimePicker()
         }
@@ -34,11 +45,9 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
     }
 
     private fun openTimePicker() {
-        val isSystem24Hour = is24HourFormat(requireContext())
-        val clockFormat = if (isSystem24Hour) TimeFormat.CLOCK_12H else TimeFormat.CLOCK_24H
 
         val picker = MaterialTimePicker.Builder()
-            .setTimeFormat(clockFormat)
+            .setTimeFormat(TimeFormat.CLOCK_12H)
             .setHour(12)
             .setMinute(0)
             .setTitleText("SELECT NOTIFICATION TIME")
@@ -50,26 +59,50 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
             val h = picker.hour
             val min = picker.minute
 
-            if (min < 10) {
-                minString = "0$min"
+            if(h > 12) {
+                timeExpect.text = String.format("%02d", picker.hour - 12) + ":" + String.format("%02d", picker.minute) + " PM"
             } else {
-                minString = "$min"
+                timeExpect.text = String.format("%02d", picker.hour) + ":" + String.format("%02d", picker.minute) + " AM"
             }
 
-            if (h < 10) {
-                hString = "0$h"
-            } else {
-                hString = "$h"
-            }
+            calendar = Calendar.getInstance()
+            calendar[Calendar.HOUR_OF_DAY] = picker.hour
+            calendar[Calendar.MINUTE] = picker.minute
+            calendar[Calendar.SECOND] = 0
+            calendar[Calendar.MILLISECOND] = 0
+
 
             if (h != null && min != null) {
                 setTimeButton.text = "change time"
+                setAlarm()
             }
-            requireView().timeExpect.text = "$hString:$minString"
+            requireView().timeExpect.text = "$h:$min"
         }
 
         requireView()
 
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name: CharSequence = "DOESNTMATTER"
+            val description = "LAST TIME AT ENDAVA"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("foxandroid", name, importance)
+            channel.description = description
+            val notificationManager = requireActivity().getSystemService(NotificationManager::class.java)
+
+            notificationManager.createNotificationChannel(channel)
+
+        }
+    }
+
+    private fun setAlarm() {
+        alarmManager = requireActivity().getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, AlarmReceiver::class.java)
+
+        pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
     }
 
 }
